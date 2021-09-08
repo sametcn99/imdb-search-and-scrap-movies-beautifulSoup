921fb335e595996f926127067e06bf9d60e4691a
Samet <57061347+sametcn99@users.noreply.github.com>
2021-09-08 14:20:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -158,9 +158,9 @@ if __name__ == "__main__":
         exc_type, exc_value, exc_traceback = sys.exc_info()
         print(repr(traceback.format_tb(exc_traceback)))
         print(f"An error occured. \nHere is Error message: {e}\nRestarted program.\n")
-        os.system("python test.py")
+        os.system("python main.py")
     except Exception as e:
         exc_type, exc_value, exc_traceback = sys.exc_info()
         print(repr(traceback.format_tb(exc_traceback)))
         print(f"Something happened.\n{e}\nRestarted program.")
-        os.system("python test.py")
+        os.system("python main.py")
